00000050cd42ab567c9d4ec92f06931612d6da7f
Ito, Kai <Kai.Ito@zuehlke.com>
2019-07-25 15:44:38 +0200

    Add FSharp literate for makrdown parsing

--- src/App/Program.fs
+++ src/App/Program.fs
@@ -7,6 +7,12 @@ open Microsoft.Extensions.DependencyInjection
 open Giraffe
 
 open App.Views
+open FSharp.Literate
+open System.Text
+open Giraffe.GiraffeViewEngine
+open Microsoft.Extensions.FileProviders
+open System.IO
+open Microsoft.AspNetCore.Http
 
 // #353535
 // #3c6e71
@@ -14,11 +20,33 @@ open App.Views
 // #D9D9D9
 // #284B63
 
+let getHtml () =
+    let listy = """
+    ### Formatting demo
+    let test = ["one";"two";"three"]
+    (*** include-value:test ***)"""
+
+    let docOl = Literate.ParseMarkdownString(listy)
+    let htmlString = Literate.WriteHtml(docOl)
+    Text htmlString
+
 let webApp =
     choose [
-        route "/" >=> (Home.view |> htmlView) ]
+        route "/" >=> (Home.view [] |> htmlView)
+        route "/test" >=> ([getHtml ()] |> Home.view |> htmlView)]
 
 let configureApp (app : IApplicationBuilder) =
+    app.UseStaticFiles() |> ignore
+    //app.UseStaticFiles(StaticFileOptions(RequestPath = PathString("/wwwroot/js"), FileProvider = new PhysicalFileProvider(Directory.GetCurrentDirectory()))) |> ignore
+    //app.UseStaticFiles(StaticFileOptions(RequestPath = PathString("/wwwroot/css"), FileProvider = new PhysicalFileProvider(Directory.GetCurrentDirectory()))) |> ignore
+
+    app.UseDirectoryBrowser(DirectoryBrowserOptions
+        (
+            FileProvider = new PhysicalFileProvider(
+                Path.Combine(Directory.GetCurrentDirectory(), "wwwroot")),
+            RequestPath = PathString("")
+        )) |> ignore
+
     app.UseGiraffe webApp
 
 let configureServices (services : IServiceCollection) =
@@ -28,6 +56,7 @@ let configureServices (services : IServiceCollection) =
 let main _ =
     WebHostBuilder()
         .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
         .Configure(Action<IApplicationBuilder> configureApp)
         .ConfigureServices(configureServices)
         .Build()
--- src/App/Views/Home.fs
+++ src/App/Views/Home.fs
@@ -4,14 +4,16 @@ namespace App.Views
 module Home =
     open Giraffe.GiraffeViewEngine
 
-    let view =
+    let view content =
         html [ _style "background-color: #353535" ] [
             head [] [
                 title [] [ str "Hashset" ]
+                link [ _rel "stylesheet"; _type "text/css"; _href "css/styles.css" ]
             ]
             body [] [
-                div [ _style "color: #3c6e71; font-size: 15rem; width: 100%; height: auto; bottom: 0px; top: 0px; left: 0; position: absolute" ] [
-                    div [ _style "position: absolute; width: 56%; top: 50%; left: 50%; transform: translate(-50%, -50%); font-family: Tahoma" ] [ str "Coming Soon" ]
+                div [ _style "color: #3c6e71; width: 100%; height: auto; bottom: 0px; top: 0px; left: 0; position: absolute" ] [
+                    div [ _style "position: absolute; width: 56%; top: 50%; left: 50%; transform: translate(-50%, -50%);" ] content
                 ]
             ]
+            script [ _async; _defer; _src "js/tips.js"; _type "text/javascript" ] []
         ]
