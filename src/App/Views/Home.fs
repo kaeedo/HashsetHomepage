000000707fa5533216cc0f775d958bb345395670
Ito, Kai <Kai.Ito@zuehlke.com>
2019-07-30 11:13:42 +0200

    Add basic layout styles

--- src/App/Views/Home.fs
+++ src/App/Views/Home.fs
@@ -4,16 +4,20 @@ namespace App.Views
 module Home =
     open Giraffe.GiraffeViewEngine
 
+    let private pageTitle = "Hashset"
+
     let view content =
-        html [ _style "background-color: #353535" ] [
+        html [ ] [
             head [] [
-                title [] [ str "Hashset" ]
+                title [] [ str pageTitle ]
                 link [ _rel "stylesheet"; _type "text/css"; _href "css/styles.css" ]
             ]
-            body [] [
-                div [ _style "color: #3c6e71; width: 100%; height: auto; bottom: 0px; top: 0px; left: 0; position: absolute" ] [
-                    div [ _style "position: absolute; width: 56%; top: 50%; left: 50%; transform: translate(-50%, -50%);" ] content
+            body [ _class "Main-body" ] [
+                header [ _class "Main-header" ] [ Text pageTitle ]
+                nav [ _class "Main-sideNav" ] [
+                    span [] [ Text "eabrg klsrhje gkjhesr gkjhe" ]
                 ]
+                div [ _class "Main-content" ] content
             ]
             script [ _async; _defer; _src "js/tips.js"; _type "text/javascript" ] []
         ]
