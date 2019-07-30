00000080eff2c701b156a5f1b9457a107cd50157
Ito, Kai <Kai.Ito@zuehlke.com>
2019-07-30 16:20:06 +0200

    Implement better layout

--- src/App/Views/Home.fs
+++ src/App/Views/Home.fs
@@ -13,11 +13,22 @@ module Home =
                 link [ _rel "stylesheet"; _type "text/css"; _href "css/styles.css" ]
             ]
             body [ _class "Main-body" ] [
-                header [ _class "Main-header" ] [ Text pageTitle ]
-                nav [ _class "Main-sideNav" ] [
-                    span [] [ Text "eabrg klsrhje gkjhesr gkjhe" ]
+                header [ _class "Main-header" ] [
+                    div [ _class "Main-headerTitle" ] [
+                        h1 [] [ Text pageTitle ]
+                    ]
                 ]
-                div [ _class "Main-content" ] content
+                section [ _class "Main-section" ] [
+                    nav [ _class "Main-sideNav" ] [
+                        ul [ _class "Main-links Card" ] [
+                            li [] [ Text "Home" ]
+                            li [] [ Text "About" ]
+                            li [] [ Text "Projects" ]
+                            li [] [ Text "Github" ]
+                        ]
+                    ]
+                    article [ _class "Main-content Card" ] content
+                ]
             ]
             script [ _async; _defer; _src "js/tips.js"; _type "text/javascript" ] []
         ]
