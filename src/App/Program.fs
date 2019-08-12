0000016090a2ad7fc9b7f46fd760cf4530e0dec1
Ito, Kai <Kai.Ito@zuehlke.com>
2019-08-12 11:04:34 +0200

    Start implementing pages

--- src/App/Program.fs
+++ src/App/Program.fs
@@ -39,15 +39,19 @@ module App =
     let configureServices (services : IServiceCollection) =
         services.AddGiraffe() |> ignore
 
+    // [<EntryPoint>]
+    // let main _ =
+    //     WebHostBuilder()
+    //         .UseKestrel()
+    //         .UseContentRoot(Directory.GetCurrentDirectory())
+    //         .Configure(Action<IApplicationBuilder> configureApp)
+    //         .ConfigureServices(configureServices)
+    //         .Build()
+    //         .Run()
+    //     0
     [<EntryPoint>]
     let main _ =
-        WebHostBuilder()
-            .UseKestrel()
-            .UseContentRoot(Directory.GetCurrentDirectory())
-            .Configure(Action<IApplicationBuilder> configureApp)
-            .ConfigureServices(configureServices)
-            .Build()
-            .Run()
+        Reader.write |> ignore
         0
 
 // https://codeasashu.github.io/hcz-jekyll-blog/jekyll/2016/06/04/welcome-to-jekyll.html
