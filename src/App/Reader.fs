91a930e8d24de07246e4911e2959046d7495a16e
Kai Ito <kai.ito@gmail.com>
2019-08-31 18:45:58 +0200

    Save JSON

--- src/App/Reader.fs
+++ src/App/Reader.fs
@@ -4,6 +4,13 @@ open FSharp.Literate
 open System.IO
 open FSharp.Markdown
 open System
+open System.Text.Json
+open System.Text.Json.Serialization
+
+type ParsedDocument =
+    { Title: string
+      Document: string
+      Tooltips: string }
 
 module Reader =
     let (++) a b = Path.Combine(a,b)
@@ -21,15 +28,16 @@ module Reader =
         let srcDir = personalDir ++ "hashset" ++ "source"
         let parsedDir = personalDir ++ "hashset" ++ "parsed"
 
+        Directory.CreateDirectory(srcDir) |> ignore
+        Directory.CreateDirectory(parsedDir) |> ignore
+
         let parsed = Literate.ProcessMarkdown(srcDir ++ "./Power.md", generateAnchors = true)
 
-        let pageTitle =
-            parsed.Parameters
-            |> getContent "page-title"
+        let parsedDocument = { Title = parsed.Parameters |> getContent "page-title"; Document = parsed.Parameters |> getContent "document"; Tooltips = parsed.Parameters |> getContent "tooltips" }
+        use outFile = File.CreateText(parsedDir ++ "power.json")
 
-        let content =
-            parsed.Parameters
-            |> getContent "document"
+        outFile.Write(JsonSerializer.Serialize<ParsedDocument>(parsedDocument))
+        outFile.Flush()
 
         //Text htmlString
         parsed.ContentTag.ToString()
