000001805b9084c1fca14a9d4dd74349b367042c
Kai Ito <kai.ito@gmail.com>
2019-08-31 18:45:58 +0200

    Save JSON

--- src/App/Program.fs
+++ src/App/Program.fs
@@ -39,20 +39,20 @@ module App =
     let configureServices (services : IServiceCollection) =
         services.AddGiraffe() |> ignore
 
-    // [<EntryPoint>]
-    // let main _ =
-    //     WebHostBuilder()
-    //         .UseKestrel()
-    //         .UseContentRoot(Directory.GetCurrentDirectory())
-    //         .Configure(Action<IApplicationBuilder> configureApp)
-    //         .ConfigureServices(configureServices)
-    //         .Build()
-    //         .Run()
-    //     0
     [<EntryPoint>]
     let main _ =
-        Reader.write |> ignore
+        WebHostBuilder()
+            .UseKestrel()
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .Configure(Action<IApplicationBuilder> configureApp)
+            .ConfigureServices(configureServices)
+            .Build()
+            .Run()
         0
+    (* [<EntryPoint>]
+    let main _ =
+        Reader.write |> ignore
+         *)0
 
 // https://codeasashu.github.io/hcz-jekyll-blog/jekyll/2016/06/04/welcome-to-jekyll.html
 (*
