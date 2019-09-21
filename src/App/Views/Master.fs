3c9bc30b66a8b9dcd85930c9e79219517a970038
Kai Ito <kai.ito@gmail.com>
2019-09-21 22:44:51 +0200

    Add above the fold CSS

--- src/App/Views/Master.fs
+++ src/App/Views/Master.fs
@@ -17,13 +17,117 @@ module Master =
         html [ ] [
             head [] [
                 title [] [ str <| sprintf "%s - Hashset.dev" masterData.PageTitle ]
-                link [ _rel "stylesheet"; _type "text/css"; _href "/css/styles.css" ]
+                style [] [
+                    str "
+                    html {
+                      overflow-y: scroll;
+                      font-family: var(Arial, sans-serif);
+                    }
+
+                    a {
+                      text-decoration: none;
+                    }
+
+                    .Main-body {
+                      background: #eee;
+                      box-sizing: border-box;
+                    }
+
+                    .Main-header {
+                      position: fixed;
+                      color: #f2f2f2;
+
+                      display: flex;
+                      justify-content: space-between;
+                      width: calc(100% - 8px);
+
+                      transition: background-color 0.5s;
+                      transition: color 0.1s;
+                      backface-visibility: hidden;
+                      background: transparent;
+
+                      margin-left: -8px;
+                      padding: 8px;
+                      font-size: 20pt;
+                      font-weight: 700;
+                    }
+                    .Main-headerNav {
+                      display: grid;
+                      grid-template-columns: 1fr 1fr 1fr;
+                      grid-column-gap: 2rem;
+                      font-weight: normal;
+                      padding-right: 8px;
+                    }
+
+                    @media screen and (max-width: 667px) {
+                        .Main-headerNav {
+                            grid-column-gap: 1rem;
+                          }
+                      }
+
+                    .Main-headerLink {
+                      color: inherit;
+                    }
+
+                    #headerBackground {
+                      position: absolute;
+                      top: 0;
+                      z-index: -54654654654;
+
+                      width: 100%;
+                      background-color: #2c2e43;
+                      background-size: cover;
+
+                      height: 300px;
+                      margin-left: calc(-1 * 8px);
+                      margin-right: calc(-1 * 8px);
+                    }
+
+                    .Main-titleContainer {
+                      margin-top: calc(-1 * 8px);
+                      margin-left: calc(-1 * 8px);
+                      margin-right: calc(-1 * 8px);
+                      margin-bottom: 2rem;
+                      display: flex;
+                      padding: 7rem 0;
+                    }
+                    .Main-title {
+                      margin: auto;
+                      padding: 8px;
+                      display: flex;
+                      flex-direction: column;
+                      justify-content: space-between;
+                      color: #f2f2f2;
+                    }
+                    .Main-postTitle {
+                      font-size: 42pt;
+                      font-weight: 700;
+                      max-width: 80rem;
+                    }
+
+                    @media screen and (max-width: 667px) {
+                        .Main-postTitle {
+                            font-size: 22pt;
+                          }
+                      }
+                    .Main-section {
+                      margin: auto;
+                    }
+                    .PostContents {
+                      display: flex;
+                      flex-direction: column;
+                      margin: auto;
+                      max-width: 80rem;
+                    }
+                    "
+                ]
+                //link [ _rel "stylesheet"; _type "text/css"; _href "/css/styles.css" ]
                 meta [ _charset "utf-8" ]
                 meta [ _name "viewport"; _content "width=device-width, initial-scale=1.0" ]
             ]
             body [ _class "Main-body" ] [
                 header [ _class "Main-header" ] [
-                    span [] [ str "Kai Itof" ]
+                    span [] [ str "Kai Ito" ]
                     nav [ _class "Main-headerNav" ] [
                         a [ _class "Main-headerLink"; _href "/" ] [
                             str "Home"
@@ -51,7 +155,7 @@ module Master =
                     content
                 ]
             ]
-            script [ _async; _defer; _src "/js/particles.min.js"; _type "text/javascript"; ] []
+            //script [ _async; _defer; _src "/js/particles.min.js"; _type "text/javascript"; ] []
             script [ _async; _defer; _src "/js/tips.js"; _type "text/javascript" ] []
             script [ _async; _defer; _src "/js/main.js"; _type "text/javascript"; ] []
         ]
