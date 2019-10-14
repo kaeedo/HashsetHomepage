00000720b55b4b50aee06eecb1c5c38a806f0d6f
Kai Ito <kai.ito@gmail.com>
2019-10-14 21:27:37 +0200

    Rename post to Article. Add DataAccess project

---
+++ src/DataAccess/Library.fs
@@ -0,0 +1,29 @@
+﻿namespace DataAccess
+
+open Rezoom
+open Rezoom.SQL
+open Rezoom.SQL.Synchronous
+open Rezoom.SQL.Migrations
+open System
+
+type MyModel = SQLModel<".">
+
+type MyQuery = SQL<"""
+    select * FROM Articles
+    WHERE Id = @id
+""">
+
+[<RequireQualifiedAccess>]
+module Db =
+    let migrate () =
+        let config =
+            { MigrationConfig.Default with
+                LogMigrationRan = fun m -> printfn "Ran migration: %s" m.MigrationName }
+
+        MyModel.Migrate(config)
+
+    let a id =
+        use connection = new ConnectionContext()
+        let users = MyQuery.Command(id).Execute(connection)
+        users
+        |> Seq.head
