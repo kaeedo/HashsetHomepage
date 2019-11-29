cee0872e0f0dca57589f1586cffdd418d0a9a862
Ito, Kai <Kai.Ito@zuehlke.com>
2019-11-29 10:35:56 +0100

    Style commento box

--- src/App/Views/Article.fs
+++ src/App/Views/Article.fs
@@ -15,20 +15,19 @@ module Article =
             hr [ _class "ArticleEnd" ]
 
             div [ _class "ArticleComments" ] [
-                div [ _class "ArticleComments-container" ] [
-                    if not shouldLoadComments then
-                        a [ _id "loadComments"; _href <| sprintf "/article/%i?loadComments=true#commento" parsedDocument.Id ] [ str "Load comments" ]
-                ]
+                if not shouldLoadComments then
+                    div [ _class "ArticleComments-container" ] [
+                        a [ _class "ArticleComments-loadComments"; _href <| sprintf "/article/%i?loadComments=true#commento" parsedDocument.Id ] [ str "Load comments" ]
+                    ]
                 div [ _id "commento" ] []
             ]
 
-            script [
-                _defer
-                _async
-                _data "no-fonts" "true"
-                _data "css-override" "/css/commentoOverride.css"
-                _data "auto-init" (shouldLoadComments.ToString().ToLowerInvariant())
-                _src "https://commento.hashset.dev/js/commento.js"
-            ] []
-            script [ _async; _defer; _src "/js/article.js"; _type "text/javascript"; ] []
+            if shouldLoadComments then
+                script [
+                    _defer
+                    _async
+                    _data "no-fonts" "true"
+                    _data "css-override" "/css/commentoOverride.css"
+                    _src "https://commento.hashset.dev/js/commento.js"
+                ] []
         ]
