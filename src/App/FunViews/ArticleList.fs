b5e5065625cd6918050e4ef1f67f60f4b511b3da
Kai Ito <kai.ito@valora.com>
2023-11-28 09:38:55 +0100

    More better last elements logic

--- src/App/FunViews/ArticleList.fs
+++ src/App/FunViews/ArticleList.fs
@@ -11,33 +11,9 @@ let private articleCard (articles: ArticleStub list) idx (stub: ArticleStub) =
         | 0 -> "col-span-6"
         | x when x < 4 -> "col-span-2 row-span-2"
         | x when x < 6 -> "col-span-3 row-span-2"
+        | x when x = articles.Length - 1 && leftOver = 1 -> "col-span-6 row-span-2"
+        | x when x >= articles.Length - 2 && leftOver = 2 -> "col-span-3 row-span-2"
         | _ -> "col-span-2 row-span-1 h-72"
-
-    let span =
-        let currentArticle = articles[idx]
-
-        let isLastThreeArticle =
-            articles
-            |> List.skip (articles.Length - 3)
-            |> List.contains currentArticle
-
-        printfn $"idx: {idx}, isLastThree: {isLastThreeArticle}, leftOver: {leftOver}"
-
-        if leftOver = 1 then
-            if articles.Length - 1 = idx then
-                span.Replace("col-span-2", "col-span-6")
-            else
-                span
-        elif leftOver = 2 then
-            if
-                articles.Length - 1 = idx
-                || articles.Length - 2 = idx
-            then
-                span.Replace("col-span-2", "col-span-3")
-            else
-                span
-        else
-            span
 
     div {
         class' $"{span} border border-gray-900 p-4 overflow-hidden"
