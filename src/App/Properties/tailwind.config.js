512a2be6dfccc9e1f27ed701baf422857acf0f3b
Kai Ito <kai.ito@valora.com>
2023-11-29 15:16:55 +0100

    Start messing with colors

--- src/App/Properties/tailwind.config.js
+++ src/App/Properties/tailwind.config.js
@@ -1,8 +1,32 @@
 /** @type {import('tailwindcss').Config} */
+// https://paletton.com/#uid=7380M0kAMsLmXBktD-oEtnhKhhT
 module.exports = {
-    content: ["../**/*.fs", "../WebRoot/js/**/*.js"],
-    theme: {
-        extend: {},
+  content: ["../**/*.fs", "../WebRoot/js/**/*.js"],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      "turquoise-100": "#34ba91",
+      "turquoise-200": "#0db482",
+      "turquoise-300": "#00976a",
+      "turquoise-400": "#007a55",
+      "turquoise-500": "#005e42",
+      "blue-100": "#4858BF",
+      "blue-200": "#2439BA",
+      "blue-300": "#11249D",
+      "blue-400": "#0C1C7F",
+      "blue-500": "#071362",
+      "gold-100": "#FFCB48",
+      "gold-200": "#FFBC13",
+      "gold-300": "#E5A400",
+      "gold-400": "#B98500",
+      "gold-500": "#8E6600",
+      "orange-100": "#FF8848",
+      "orange-200": "#FF6513",
+      "orange-300": "#E55000",
+      "orange-400": "#B94100",
+      "orange-500": "#8E3200",
     },
-    plugins: [],
-}
+  },
+  plugins: [],
+};
