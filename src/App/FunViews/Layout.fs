000021906345c53c8517c6d97ef07d3119b84e5f
Kai Ito <kai.ito@valora.com>
2023-11-29 15:16:55 +0100

    Start messing with colors

--- src/App/FunViews/Layout.fs
+++ src/App/FunViews/Layout.fs
@@ -11,15 +11,11 @@ type Layout =
                 chartsetUTF8
                 viewport "width=device-width, initial-scale=1.0"
                 defaultArg headerNode (title { "Hashset" })
-#if DEBUG
-                script { src "https://cdn.tailwindcss.com/" }
-#else
                 stylesheet "/css/tailwind.css"
-#endif
             }
 
             body {
-                class' "container mx-auto p-10"
+                class' "container mx-auto p-10 bg-orange-300"
                 bodyNode
 
                 script { src "https://unpkg.com/htmx.org@1.9.9" }
