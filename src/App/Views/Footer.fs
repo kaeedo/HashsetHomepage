f945f4ae58e3233cb6b02a62290e681f9f2f7b2e
Kai Ito <kai.ito@valora.com>
2023-12-05 15:21:02 +0100

    About page

--- src/App/Views/Footer.fs
+++ src/App/Views/Footer.fs
@@ -15,111 +15,87 @@ let view () =
 
             childContent [
                 li {
-                    div {
-                        class' "flex mb-3"
+                    class' "flex mb-3"
 
+                    div { class' "w-10 h-0" }
+                    div { sprintf "Blog Posts Copyright © %i" DateTime.Now.Year }
+                }
+                li {
+                    class' "flex mb-3"
+
+                    childContent [
                         div { class' "w-10 h-0" }
-                        div { sprintf "Blog Posts Copyright © %i" DateTime.Now.Year }
-                    }
+                        div {
+                            childContent [
+                                span { "Source Code available on " }
+
+                                a {
+                                    class' "underline"
+                                    target "_blank"
+                                    href "https://codeberg.org/CubeOfShame/Hashset"
+                                    "Codeberg"
+                                }
+
+                                span { " or " }
+
+                                a {
+                                    class' "underline"
+                                    target "_blank"
+                                    href "https://github.com/kaeedo/HashsetHomepage"
+                                    "GitHub"
+                                }
+                            ]
+                        }
+                    ]
                 }
                 li {
+                    class' "flex mb-3"
+
                     div {
-                        class' "flex mb-3"
+                        class' "w-10"
 
-                        childContent [
-                            div { class' "w-10 h-0" }
-                            div {
-                                childContent [
-                                    span { "Source Code available on " }
+                        i { html.raw Svg.mastodon }
+                    }
 
-                                    a {
-                                        class' "underline"
-                                        target "_blank"
-                                        href "https://codeberg.org/CubeOfShame/Hashset"
-                                        "Codeberg"
-                                    }
+                    div {
+                        a {
+                            rel "me"
+                            href "https://mstdn.social/@kaeedo"
 
-                                    span { " or " }
-
-                                    a {
-                                        class' "underline"
-                                        target "_blank"
-                                        href "https://github.com/kaeedo/HashsetHomepage"
-                                        "GitHub"
-                                    }
-                                ]
+                            span {
+                                class' "underline"
+                                "Follow me"
                             }
-                        ]
+
+                            " on Mastodon"
+                        }
                     }
                 }
                 li {
-                    div {
-                        class' "flex mb-3"
+                    class' "flex"
 
+                    childContent [
                         div {
                             class' "w-10"
 
-                            i {
-                                html.raw
-                                    """<svg width="32px" height="32px" fill="#6364FF" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                        <rect height="14" width="17" x="3" y="3" fill="#fff" />
-                                        <path d="M23.193 7.879c0-5.206-3.411-6.732-3.411-6.732C18.062.357 15.108.025 12.041 0h-.076c-3.068.025-6.02.357-7.74 1.147 0 0-3.411 1.526-3.411 6.732 0 1.192-.023 2.618.015 4.129.124 5.092.934 10.109 5.641 11.355 2.17.574 4.034.695 5.535.612 2.722-.15 4.25-.972 4.25-.972l-.09-1.975s-1.945.613-4.129.539c-2.165-.074-4.449-.233-4.799-2.891a5.499 5.499 0 0 1-.048-.745s2.125.52 4.817.643c1.646.075 3.19-.097 4.758-.283 3.007-.359 5.625-2.212 5.954-3.905.517-2.665.475-6.507.475-6.507zm-4.024 6.709h-2.497V8.469c0-1.29-.543-1.944-1.628-1.944-1.2 0-1.802.776-1.802 2.312v3.349h-2.483v-3.35c0-1.536-.602-2.312-1.802-2.312-1.085 0-1.628.655-1.628 1.944v6.119H4.832V8.284c0-1.289.328-2.313.987-3.07.68-.758 1.569-1.146 2.674-1.146 1.278 0 2.246.491 2.886 1.474L12 6.585l.622-1.043c.64-.983 1.608-1.474 2.886-1.474 1.104 0 1.994.388 2.674 1.146.658.757.986 1.781.986 3.07v6.304z"/>
-                                    </svg>"""
-                            }
+                            i { html.raw Svg.rss }
                         }
-
-                        div {
+                        span {
                             a {
-                                rel "me"
-                                href "https://mstdn.social/@kaeedo"
+                                class' "underline"
+                                href "/rss"
+                                "RSS"
+                            }
 
-                                span {
-                                    class' "underline"
-                                    "Follow me"
-                                }
+                            " | "
 
-                                " on Mastodon"
+                            a {
+                                class' "underline"
+                                href "/atom"
+                                "ATOM"
                             }
                         }
-                    }
-                }
-                li {
-                    div {
-                        class' "flex"
-
-                        childContent [
-                            div {
-                                class' "w-10"
-
-                                i {
-                                    html.raw
-                                        """<svg role="img" xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 8 8">
-                                            <circle cx="2" cy="6" r="1" class="symbol" fill="#2c2e43"/>
-                                            <path fill="#2c2e43" d="M1 4a3 3 0 013 3h1a4 4 0 00-4-4z" class="symbol"/>
-                                            <path fill="#2c2e43" d="M1 2a5 5 0 015 5h1a6 6 0 00-6-6z" class="symbol"/>
-                                        </svg>"""
-
-                                }
-                            }
-                            div {
-                                childContent [
-                                    a {
-                                        class' "underline"
-                                        href "/rss"
-                                        "RSS"
-                                    }
-
-                                    span { " | " }
-
-                                    a {
-                                        class' "underline"
-                                        href "/atom"
-                                        "ATOM"
-                                    }
-                                ]
-                            }
-                        ]
-                    }
+                    ]
                 }
             ]
         }
--- src/App/Views/Pages/About.fs
+++ src/App/Views/Pages/About.fs
@@ -5,6 +5,8 @@ open Fun.Blazor
 open App.Views.Components
 open App.Views
 
+
+
 let private html =
     html.fragment [
         div {
@@ -12,8 +14,12 @@ let private html =
 
             childContent [
                 html.fragment [
-                    h1 { "Hello, I'm Kai Ito" }
+                    h4 {
+                        class' "text-lg mb-4"
+                        "Hello, I'm Kai Ito"
+                    }
                     p {
+                        class' "mb-2"
                         "I am a Software Developer at "
 
                         a {
@@ -32,26 +38,94 @@ let private html =
                 |> Card.simple "col-span-2"
 
                 html.fragment [
-                    h4 { "Come find me at the following sites" }
-                    //     """
-                    //     <svg width="16" height="16" version="1.1" viewBox="0 0 4.2333 4.2333" xmlns="http://www.w3.org/2000/svg" xmlns:cc="http://creativecommons.org/ns#" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:xlink="http://www.w3.org/1999/xlink">
-                    //     <title>Codeberg logo</title>
-                    //     <defs>
-                    //     <linearGradient id="a" x1="42519" x2="42575" y1="-7078.8" y2="-6966.9" gradientUnits="userSpaceOnUse">
-                    //     <stop stop-color="#2185d0" stop-opacity="0" offset="0"/>
-                    //     <stop stop-color="#2185d0" stop-opacity=".3" offset=".49517"/>
-                    //     <stop stop-color="#2185d0" stop-opacity=".3" offset="1"/>
-                    //     </linearGradient>
-                    //     </defs>
-                    //     <g transform="matrix(.065514 0 0 .065514 -2.2324 -1.4318)">
-                    //     <path transform="matrix(.37058 0 0 .37058 -15690 2662.1)" d="m42519-7078.8a0.76087 0.56792 0 0 0-0.738 0.6739l33.586 125.89a87.182 87.182 0 0 0 39.381-33.764l-71.565-92.52a0.76087 0.56792 0 0 0-0.664-0.2793z" fill="url(#a)" stop-color="#000000" style="font-variation-settings:normal;paint-order:stroke markers fill"/>
-                    //     <path transform="matrix(1.4006 0 0 1.4006 -15690 2662.1)" d="m11249-1883.7c-12.74 0-23.067 10.328-23.067 23.067 0 4.3335 1.22 8.5795 3.522 12.251l19.232-24.864c0.138-0.1796 0.486-0.1796 0.624 0l19.233 24.865c2.302-3.6721 3.523-7.9185 3.523-12.252 0-12.74-10.327-23.067-23.067-23.067z" fill="#2185d0" stop-color="#000000" stroke-width="17.006" style="paint-order:markers fill stroke"/>
-                    //     </g>
-                    //     </svg>
-                    //     """
-                    // ]
-                    |> Card.simple ""
+                    h4 {
+                        class' "text-lg mb-4"
+                        "Come find me at the following sites"
+                    }
+                    ul {
+                        class' "pl-4"
+
+                        li {
+                            class' "flex mb-3 items-center"
+
+                            div {
+                                class' "w-10"
+
+                                i { html.raw Svg.mastodon }
+                            }
+
+                            a {
+                                class' "underline"
+                                href "https://mstdn.social/@kaeedo"
+
+                                "Mastodon"
+                            }
+                        }
+
+                        li {
+                            class' "flex mb-3 items-center"
+
+                            div {
+                                class' "w-10"
+
+                                i { html.raw Svg.codeberg }
+                            }
+
+                            a {
+                                class' "underline"
+                                href "https://codeberg.org/CubeOfShame"
+
+                                "Codeberg"
+                            }
+                        }
+
+                        li {
+                            class' "flex mb-3 items-center"
+
+                            div {
+                                class' "w-10"
+
+                                i { html.raw Svg.github }
+                            }
+
+                            a {
+                                class' "underline"
+                                href "https://github.com/kaeedo"
+
+                                "GitHub"
+                            }
+                        }
+
+                        li {
+                            class' "flex mb-3 items-center"
+
+                            div {
+                                class' "w-10"
+
+                                i { html.raw Svg.rss }
+                            }
+
+                            span {
+                                a {
+                                    class' "underline"
+                                    href "https://github.com/kaeedo"
+
+                                    "RSS"
+                                }
+
+                                " | "
+
+                                a {
+                                    class' "underline"
+                                    href "https://github.com/kaeedo"
+
+                                    "ATOM"
+                                }
+                            }
+                        }
+                    }
                 ]
+                |> Card.simple ""
             ]
         }
     ]
