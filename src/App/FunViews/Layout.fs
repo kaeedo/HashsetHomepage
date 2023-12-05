d42429cd97514c54fc69cdbff2ec9877a090ae6c
Kai Ito <kai.ito@valora.com>
2023-12-05 09:21:39 +0100

    Add favicons

--- src/App/FunViews/Layout.fs
+++ src/App/FunViews/Layout.fs
@@ -4,6 +4,7 @@ open Fun.Blazor
 open Fun.Htmx
 
 type Layout =
+
     static member Create(bodyNode: NodeRenderFragment, ?headerNode: NodeRenderFragment) =
         html' {
             doctype "html"
@@ -13,6 +14,51 @@ type Layout =
                 viewport "width=device-width, initial-scale=1.0"
                 defaultArg headerNode (title { "Hashset" })
                 stylesheet "/css/tailwind.css"
+
+                link {
+                    rel "android-chrome-192x192"
+                    size "180x180"
+                    href "/android-chrome-192x192.png"
+                }
+
+                link {
+                    rel "android-chrome-512x512"
+                    size "180x180"
+                    href "/android-chrome-512x512.png"
+                }
+
+                link {
+                    rel "apple-touch-icon"
+                    size "180x180"
+                    href "/apple-touch-icon.png"
+                }
+
+                link {
+                    rel "icon"
+                    size "32x32"
+                    href "/favicon-32x32.png"
+                }
+
+                link {
+                    rel "icon"
+                    size "16x16"
+                    href "/favicon-16x16.png"
+                }
+
+                link {
+                    rel "manifest"
+                    href "/site.webmanifest"
+                }
+
+                meta {
+                    name "msapplication-TileColor"
+                    content "#da532c"
+                }
+
+                meta {
+                    name "theme-color"
+                    content "#ffffff"
+                }
             }
 
             body {
