5ff1992c304053b7121782a42e0ab7bff63464c7
Kai Ito <kai.ito@valora.com>
2023-12-06 09:50:15 +0100

    Fix nav

--- src/App/Views/Components/TagList.fs
+++ src/App/Views/Components/TagList.fs
@@ -3,6 +3,7 @@ module App.Views.Components.TagList
 
 open Fun.Blazor
 open Model
+open System
 
 let simple (tags: Tag list) =
     div {
@@ -11,11 +12,13 @@ let simple (tags: Tag list) =
         childContent (
             tags
             |> List.map (fun t ->
+                let encoded = Web.HttpUtility.UrlEncode(t.Name, Text.Encoding.ASCII)
+
                 a {
                     class'
                         "relative border-2 py-1 px-2 min-w-max bg-blue drop-shadow-[4px_4px_0px_#dd7dff] hover:bg-purple"
 
-                    href "#"
+                    href $"/articles?tag=%s{encoded}"
 
                     t.Name
                 })
