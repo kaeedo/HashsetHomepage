5f2f8e178b93e160a7c08828bfd02d2228f9778b
Kai Ito <kai.ito@valora.com>
2023-12-08 16:11:49 +0100

    Introduce SqlHydra as ORM, and Grate as migration tool

---
+++ migrations.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ -z "$1" ]; then
+  echo "Usage:"
+  echo ""
+  echo "  $ $0 name_of_migration_file"
+  exit 1
+fi
+
+path="src/DataAccess.Hydra/Migrations/up/$(date -u "+%Y%m%d%H%M%S")_$1.sql"
+touch "$path"
+
+echo "Migration created: $path"
+
+# dotnet grate -c "connectionString" -f src/DataAccess.Hydra/Migrations --dt postgresql -t
